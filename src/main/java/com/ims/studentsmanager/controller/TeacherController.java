c0bfdab509e4c0c395adb384c2e0c6e46cac6a09
jaguargirls <nbobu13@gmail.com>
2021-10-16 01:50:49 +0300

    Added RequestMapping and Crossorigin

--- src/main/java/com/ims/studentsmanager/controller/TeacherController.java
+++ src/main/java/com/ims/studentsmanager/controller/TeacherController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/teachers")
+@CrossOrigin("http://localhost:3000/")
 public class TeacherController {
     private final TeacherService teacherService;
 
