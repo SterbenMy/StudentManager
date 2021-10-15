d2eab53c45d1e1a8fa484f16062159ad01a9b7c5
Cristian Macari <cristian.macari.info@gmail.com>
2021-10-15 04:05:19 +0300

    create delete course component, add on click delete button action

---
+++ src/frontend/src/components/courses/DeleteCourseComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div >
+    <el-button @click="handleDelete()">Delete</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DeleteCourseComponent",
+  // trebuie de facut props care cere acel id in view-ul principal
+  methods: {
+    //parametru id la elementul din array
+    handleDelete() {
+      this.$confirm("Are you sure to delete this course?", {
+        confirmButtonText: "Delete",
+        cancelButtonText: "Cancel",
+        customClass: "delete-message-box",
+      })
+          .then(() => {
+            //tipa sterge aici vuex  delete this.element[id];
+          })
+          .catch(() => {
+            return 0;
+          });
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
